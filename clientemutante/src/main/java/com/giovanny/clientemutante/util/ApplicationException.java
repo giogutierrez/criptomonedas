a0cf2917ce35dd16c40d056095757cbd17fff773
Giovanny Gutierrez <giovannyarturogutierrez@hotmail.com>
2022-01-25 17:23:01 -0500

    Delete clientemutante directory

--- clientemutante/src/main/java/com/giovanny/clientemutante/util/ApplicationException.java
+++
@@ -1,9 +0,0 @@
-package com.giovanny.clientemutante.util;
-
-public class ApplicationException extends RuntimeException{
-
-	public ApplicationException(String mensaje) {
-		super(mensaje);
-	}
-	
-}
